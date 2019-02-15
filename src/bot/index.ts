cc3c499cec5b2a4dda983b8c73800e45c0ecfff3
Wiljan Arias Milian <warias>
2019-02-15 18:57:05 +0100

    adding types to slack bot implementation

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -2,8 +2,10 @@ import createSlackBot from './handler/slack';
 import {getSlackBotToken} from '../config';
 import {OutgoingMessage, IncomingMessage} from './model/message';
 
+export type IncomingMessageListener = (bot: Bot, message: IncomingMessage) => void;
+
 export interface Bot {
-    addListener: (listener: (bot: Bot, message: OutgoingMessage) => void) => void;
+    addListener: (listener: IncomingMessageListener) => void;
     clearListeners: () => void;
     sendMessage: (message: OutgoingMessage, incomingMessage: IncomingMessage) => void;
     start: () => void;
--- src/bot/message-adapter/slack.ts
+++ src/bot/message-adapter/slack.ts
@@ -3,79 +3,126 @@ import {USER_MENTION, OutgoingMessage, IncomingMessage} from '../model/message';
 import {getBotDisplayName} from '../../config';
 import {SIM_IDENTIFICATION_COMMAND} from '../speech/sim-identification';
 
-export const MESSAGE_TYPE_PLAIN = 'plain';
-export const MESSAGE_TYPE_RICH = 'rich';
+export enum SlackMessageContainer {
+    PLAIN = 'plain',
+    RICH = 'rich',
+}
 
-const adaptMessage = (message: OutgoingMessage, repliedMessage: IncomingMessage) => {
+interface SlackMessageAttachmentField {
+    title: string;
+    value: string;
+    short: boolean;
+}
+
+interface SlackMessageAttachment {
+    text?: string;
+    color?: string;
+    fields?: SlackMessageAttachmentField[];
+    author_name?: string;
+    footer?: string;
+}
+
+export interface SlackMessage {
+    container: SlackMessageContainer;
+    isPrivate: boolean;
+    text: string;
+    replyOn?: string;
+    attachments?: SlackMessageAttachment[];
+}
+
+const adaptMessage = (
+    message: OutgoingMessage,
+    incomingMessage: IncomingMessage | null
+): SlackMessage | null => {
     switch (message.type) {
         case MessageType.NOTIFY_BOOTING:
         case MessageType.NOTIFY_BOOT_DONE:
             return {
-                container: MESSAGE_TYPE_PLAIN,
+                container: SlackMessageContainer.PLAIN,
                 text: `:rocket: ${message.text}`,
+                isPrivate: false,
             };
         case MessageType.ANSWER_CATALOG:
-            return {
-                container: MESSAGE_TYPE_RICH,
-                text: message.text ? message.text.replace(USER_MENTION, `@${repliedMessage.userName}`) : '',
-                replyOn: repliedMessage.channel,
-            };
+            return incomingMessage
+                ? {
+                      container: SlackMessageContainer.RICH,
+                      text: message.text
+                          ? message.text.replace(USER_MENTION, `@${incomingMessage.userName}`)
+                          : '',
+                      isPrivate: false,
+                      replyOn: incomingMessage.channel,
+                  }
+                : null;
         case MessageType.ANSWER_CATALOG_CONTENT:
             const messageText =
                 message.textLines && message.textLines.length > 0
                     ? message.textLines.reduce((text, line) => `${text}\n${line}`, '')
                     : 'No SIM cards detected.';
-            return {
-                container: MESSAGE_TYPE_PLAIN,
-                text: messageText,
-                replyOn: repliedMessage.channel,
-            };
+            return incomingMessage
+                ? {
+                      container: SlackMessageContainer.PLAIN,
+                      text: messageText,
+                      replyOn: incomingMessage.channel,
+                      isPrivate: false,
+                  }
+                : null;
         case MessageType.ANSWER_SIM_DETAILS:
-            return {
-                container: MESSAGE_TYPE_RICH,
-                text: message.text ? message.text.replace(USER_MENTION, `@${repliedMessage.userName}`) : '',
-                replyOn: repliedMessage.channel,
-            };
+            return incomingMessage
+                ? {
+                      container: SlackMessageContainer.RICH,
+                      text: message.text
+                          ? message.text.replace(USER_MENTION, `@${incomingMessage.userName}`)
+                          : '',
+                      replyOn: incomingMessage.channel,
+                      isPrivate: false,
+                  }
+                : null;
         case MessageType.ANSWER_SIM_DETAILS_CONTENT:
-            return {
-                container: MESSAGE_TYPE_RICH,
-                text: message.text,
-                attachments: message.attachments
-                    ? message.attachments.map(attachment => ({
-                          fields: attachment.fields.map(field => ({
-                              title: field.name,
-                              value: field.value,
-                              short: true,
-                          })),
-                      }))
-                    : [],
-                replyOn: repliedMessage.channel,
-            };
+            return incomingMessage
+                ? {
+                      container: SlackMessageContainer.RICH,
+                      text: message.text ? message.text : '',
+                      attachments: message.attachments
+                          ? message.attachments.map(attachment => ({
+                                fields: attachment.fields.map(field => ({
+                                    title: field.name,
+                                    value: field.value,
+                                    short: true,
+                                })),
+                            }))
+                          : [],
+                      replyOn: incomingMessage.channel,
+                      isPrivate: false,
+                  }
+                : null;
         case MessageType.NOTIFY_UNKNOWN_SIM_EXISTENCE:
-            return {
-                container: MESSAGE_TYPE_RICH,
-                isPrivate: true,
-                replyOn: repliedMessage.channel,
-                text:
-                    message.textLines && message.textLines.length > 1
-                        ? `There are *${
-                              message.textLines.length
-                          } unknown SIM cards* needing to be identified:`
-                        : 'There is an *unknown SIM* card needing to be identified:',
-                attachments: message.textLines
-                    ? message.textLines.map((line, index) => ({
-                          color: '#D3D3D3',
-                          text: line,
-                          footer: `To identify this sim, type: *${getBotDisplayName()} ${SIM_IDENTIFICATION_COMMAND} ${
-                              message.textLines && message.textLines.length > 1 ? index + 1 : ''
-                          }*`,
-                      }))
-                    : [],
-            };
+            return incomingMessage
+                ? {
+                      container: SlackMessageContainer.RICH,
+                      isPrivate: true,
+                      replyOn: incomingMessage.channel,
+                      text:
+                          message.textLines && message.textLines.length > 1
+                              ? `There are *${
+                                    message.textLines.length
+                                } unknown SIM cards* needing to be identified:`
+                              : 'There is an *unknown SIM* card needing to be identified:',
+                      attachments: message.textLines
+                          ? message.textLines.map((line, index) => ({
+                                color: '#D3D3D3',
+                                text: line,
+                                footer: `To identify this sim, type: *${getBotDisplayName()} ${SIM_IDENTIFICATION_COMMAND} ${
+                                    message.textLines && message.textLines.length > 1 ? index + 1 : ''
+                                }*`,
+                            }))
+                          : [],
+                  }
+                : null;
         case MessageType.NOTIFY_SMS_RECEIVED:
             return {
-                container: MESSAGE_TYPE_RICH,
+                container: SlackMessageContainer.RICH,
                 text: message.textLines && message.textLines.length > 0 ? message.textLines[0] : 'Empty SMS',
+                isPrivate: false,
                 attachments: [
                     {
                         author_name: ':envelope_with_arrow: New sms:',
@@ -86,42 +133,56 @@ const adaptMessage = (message: OutgoingMessage, repliedMessage: IncomingMessage)
             };
         case MessageType.FREE_TEXT_QUESTION:
             const [question] = message.textLines ? message.textLines : [''];
-            return {
-                container: MESSAGE_TYPE_PLAIN,
-                text: question,
-                replyOn: repliedMessage.channel,
-            };
+            return incomingMessage
+                ? {
+                      container: SlackMessageContainer.PLAIN,
+                      text: question,
+                      replyOn: incomingMessage.channel,
+                      isPrivate: false,
+                  }
+                : null;
         case MessageType.SINGLE_SELECTION_QUESTION:
             const [questionText, ...options] = message.textLines ? message.textLines : [''];
-            return {
-                container: MESSAGE_TYPE_RICH,
-                text: questionText,
-                replyOn: repliedMessage.channel,
-                attachments: options.map((option, index) => ({
-                    color: '#D3D3D3',
-                    text: `${index + 1}) ${option}`,
-                })),
-            };
+            return incomingMessage
+                ? {
+                      container: SlackMessageContainer.RICH,
+                      text: questionText,
+                      replyOn: incomingMessage.channel,
+                      isPrivate: false,
+                      attachments: options.map((option, index) => ({
+                          color: '#D3D3D3',
+                          text: `${index + 1}) ${option}`,
+                      })),
+                  }
+                : null;
         case MessageType.SUCCESS:
-            return {
-                container: MESSAGE_TYPE_PLAIN,
-                isPrivate: true,
-                replyOn: repliedMessage.channel,
-                text: `${
-                    message.text ? message.text.replace(USER_MENTION, `@${repliedMessage.userName}`) : ''
-                }`,
-            };
+            return incomingMessage
+                ? {
+                      container: SlackMessageContainer.PLAIN,
+                      isPrivate: true,
+                      replyOn: incomingMessage.channel,
+                      text: `${
+                          message.text
+                              ? message.text.replace(USER_MENTION, `@${incomingMessage.userName}`)
+                              : ''
+                      }`,
+                  }
+                : null;
         case MessageType.ERROR:
-            return {
-                container: MESSAGE_TYPE_PLAIN,
-                isPrivate: true,
-                replyOn: repliedMessage.channel,
-                text: `${
-                    message.text ? message.text.replace(USER_MENTION, `@${repliedMessage.userName}`) : ''
-                }`,
-            };
+            return incomingMessage
+                ? {
+                      container: SlackMessageContainer.PLAIN,
+                      isPrivate: true,
+                      replyOn: incomingMessage.channel,
+                      text: `${
+                          message.text
+                              ? message.text.replace(USER_MENTION, `@${incomingMessage.userName}`)
+                              : ''
+                      }`,
+                  }
+                : null;
         default:
-            return undefined;
+            return null;
     }
 };
 
